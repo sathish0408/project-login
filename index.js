674c2a09680e51d9a6f32f08ab6564d81f957fa8
sathish0408 <sathishallu9963@gmail.com>
2026-01-29 22:02:57 +0530

    Fix port for Render

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ app.post("/login", async (req, res) => {
 });
 
 // server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
